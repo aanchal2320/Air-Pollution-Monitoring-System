ecd3b111492feb7312f0554e89cf6dde3aa2ce9d
aanchal2320 <76009641+aanchal2320@users.noreply.github.com>
2021-09-25 13:03:40 +0530

    Update air_pollution_monitoring_system.ino

--- air_pollution_monitoring_system.ino
+++ air_pollution_monitoring_system.ino
@@ -1,21 +1,47 @@
-int redLed=12;
-int greenLed=8;
+#include <ESP8266WiFi.h>
+
+int redLed=4;
+int greenLed=5;
 int buzzer=10;
 #define analog A0
 int sensorThres=500;
-void setup() {
- pinMode(redLed,OUTPUT);
- pinMode(greenLed,OUTPUT);
- pinMode(buzzer,OUTPUT);
- pinMode(analog,INPUT);
- Serial.begin(9600);
+
+
+const char *ssid =  "Aanchal";     
+const char *pass =  "aa301209";
+
+WiFiClient client;
+ 
+void setup() 
+{
+       Serial.begin(9600);
+       delay(10);
+               
+       Serial.println("Connecting to ");
+       Serial.println(ssid); 
+ 
+       WiFi.begin(ssid, pass); 
+       while (WiFi.status() != WL_CONNECTED) 
+          {
+            delay(500);
+            Serial.print(".");
+          }
+      Serial.println("");
+      Serial.println("WiFi connected"); 
+
+      pinMode(redLed,OUTPUT);
+      pinMode(greenLed,OUTPUT);
+      pinMode(buzzer,OUTPUT);
+      pinMode(analog,INPUT);
+      Serial.begin(9600);
 }
+
 void loop() {
+ 
   int analogSensor=analogRead(analog);
   Serial.print("Pin A0: ");
   Serial.println(analogSensor);
-  if(analogSensor>
-  sensorThres)
+  if(analogSensor>sensorThres)
   {
     digitalWrite(redLed,HIGH);
     digitalWrite(greenLed,LOW);
@@ -28,4 +54,5 @@ void loop() {
     noTone(buzzer);
   }
   delay(100);
+
 }
